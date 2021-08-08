77f10ac7529a7af5b671e151b59c9b5b5ac70acf
JonhyBack <djwar0962@gmail.com>
2021-08-08 17:33:32 +0300

    First upload

---
+++ MethodFourierSeries/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace MethodFourierSeries
+{
+    public partial class Form1 : Form
+    {
+        int _degree = 15;
+        (double A, double B) _limits = (A: -4, B: 4);
+        double _epsilon = 0.001;
+        List<double> xValues = new List<double>();
+        List<double> yValues = new List<double>();
+
+        Func<double, double> Func1 = x => x;
+        Func<double, double> Func2 = x => 2 * x;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            for (double i = _limits.A; i < 0; i += _epsilon)
+            {
+                xValues.Add(i);
+                yValues.Add(Func1(i));
+            }
+
+            for (double i = 0; i < _limits.B; i += _epsilon)
+            {
+                xValues.Add(i);
+                yValues.Add(Func2(i));
+            }
+
+            chart1.ChartAreas[0].AxisX.RoundAxisValues();
+            chart1.ChartAreas[0].AxisY.RoundAxisValues();
+
+            chart1.ChartAreas[0].AxisX.Interval = 0.5;
+            chart1.ChartAreas[0].AxisY.Interval = 0.5;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            chart1.Series["Chart of the function"].Points.DataBindXY(xValues, yValues);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Resolve();
+        }
+
+        private void Resolve()
+        {
+            var result = new FourierSeries(_degree, Func1, Func2)
+            {
+                Limits = _limits,
+                Epsilon = _epsilon
+            }.GetResult();
+
+            chart1.Series["Chart of the polinom"].Points.DataBindXY(result.xValues, result.yValues);
+        }
+    }
+}
---
+++ MethodFourierSeries/FourierSeries.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodFourierSeries
+{
+    class FourierSeries
+    {
+        private double l = 4;
+        private (double A, double B) limits = (A: -4, B: 4);
+        private int _degree = 15;
+        private List<double> resultXValues;
+        private List<double> resultYValues;
+
+        private Func<double, double> Func1 = x => x;
+        private Func<double, double> Func2 = x => 2 * x;
+
+        public double Epsilon { get; set; } = 0.001;
+        public (double A, double B) Limits
+        {
+            get => limits; set
+            {
+                limits = value;
+                l = value.B;
+            }
+        }
+
+        public FourierSeries()
+        {
+            resultXValues = new List<double>();
+            resultYValues = new List<double>();
+        }
+
+        public FourierSeries(int degree) : this()
+        {
+            _degree = degree;
+        }
+
+        public FourierSeries(int degree, Func<double, double> func1, Func<double, double> func2) : this(degree)
+        {
+            Func1 = func1;
+            Func2 = func2;
+        }
+
+        public (List<double> xValues, List<double> yValues) GetResult()
+        {
+            Calculate();
+            return (xValues: resultXValues, yValues: resultYValues);
+        }
+
+        private void Calculate()
+        {
+            var a0Int1 = RightRectangle(Func1, Limits.A, 0, 1000);
+            var a0Int2 = RightRectangle(Func2, 0, Limits.B, 1000);
+            var a0 = (1d / l) * (a0Int1 + a0Int2);
+
+            double[] aValues = GetAValues();
+            double[] bValues = GetBValues();
+
+            double f(double x)
+            {
+                double result = Math.Round(a0 / 2, 4);
+
+                for (int i = 1; i <= _degree; i++)
+                {
+                    result += Math.Round(aValues[i - 1] * Math.Cos(i * x), 4);
+                    result += Math.Round(bValues[i - 1] * Math.Sin(i * x), 4);
+                }
+
+                return result;
+            };
+
+            CalculateResult(f);
+        }
+       
+        private void CalculateResult(Func<double, double> f)
+        {
+            resultXValues.Clear();
+            resultYValues.Clear();
+
+            for (double i = Limits.A; i < Limits.B; i += Epsilon)
+            {
+                resultXValues.Add(i);
+                resultYValues.Add(f(i));
+            }
+        }
+
+        private double[] GetAValues()
+        {
+            double[] aValues = new double[_degree];
+
+            for (int i = 1; i <= _degree; i++)
+            {
+                double f1(double x) => Func1(x) * Math.Cos(i * Math.PI * x / l);
+                double f2(double x) => Func2(x) * Math.Cos(i * Math.PI * x / l);
+
+                aValues[i - 1] = FindIntegral(f1, f2);
+            }
+
+            return aValues;
+        }
+
+        private double[] GetBValues()
+        {
+            double[] bValues = new double[_degree];
+
+            for (int i = 1; i <= _degree; i++)
+            {
+                double f1(double x) => Func1(x) * Math.Sin(i * Math.PI * x / l);
+                double f2(double x) => Func2(x) * Math.Sin(i * Math.PI * x / l);
+                
+                bValues[i - 1] = FindIntegral(f1, f2); ;
+            }
+
+            return bValues;
+        }
+
+        private double FindIntegral(Func<double, double> f1, Func<double, double> f2)
+        {
+            var int1 = Math.Round(RightRectangle(f1, Limits.A, 0, 1000), 4);
+            var int2 = Math.Round(RightRectangle(f2, 0, Limits.B, 1000), 4);
+
+            var result = Math.Round((1d / l), 4) * (int1 + int2);
+            return result;
+        }
+
+        private double RightRectangle(Func<double, double> f, double a, double b, int n)
+        {
+            var h = (b - a) / n;
+            var sum = 0d;
+            for (var i = 1; i <= n; i++)
+            {
+                var x = a + i * h;
+                sum += f(x);
+            }
+
+            var result = h * sum;
+            return result;
+        }
+    }
+}
